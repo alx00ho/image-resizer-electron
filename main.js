ffccee26bcb476447a24fb5760eab2f43fd67539
alx00ho <46589739+alx00ho@users.noreply.github.com>
2023-06-29 18:30:57 -0400

    Added Developer Tools Toggle

--- main.js
+++ main.js
@@ -92,6 +92,19 @@ const menu = [
         },
       ]
     : []),
+  ...(isDev
+    ? [
+        {
+          label: "Developer",
+          submenu: [
+            { role: "reload" },
+            { role: "forcereload" },
+            { type: "separator" },
+            { role: "toggledevtools" },
+          ],
+        },
+      ]
+    : []),
 ];
 
 // Respond to ipcRenderer resize
